db4b382226e7facec207c9d208814af9e525c3df
Anita Undurraga <ana.undurraga@buda.com>
2024-10-02 14:20:25 -0300

    feat: use gem naming standard

--- lib/internal_notifications.rb
+++ lib/internal_notifications.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-require_relative "internal_notifications/version"
+require_relative 'internal_notifications/version'
+require 'internal_notifications/send'
+require 'internal_notifications/pubsub_client'
 
 module InternalNotifications
   class Error < StandardError; end
-  # Your code goes here...
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "internal_notifications"
+require "internal-notifications"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
